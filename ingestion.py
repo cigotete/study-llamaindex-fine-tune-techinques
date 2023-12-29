b60c7734db70b374cfb284c84aafff32a3fa2b2a
Camilo Figueroa <cigotete@gmail.com>
2023-12-29 18:33:13 -0500

    Changed ingestion file format to load txt files.

--- ingestion.py
+++ ingestion.py
@@ -11,6 +11,7 @@ from llama_index import (
     ServiceContext,
     VectorStoreIndex,
     StorageContext,
+    SimpleDirectoryReader
 )
 from llama_index.vector_stores import PineconeVectorStore
 import pinecone
@@ -23,16 +24,12 @@ pinecone.init(
 )
 if __name__ == "__main__":
     print("Going to ingest pinecone documentation...")
-    PDFReader = download_loader("PDFReader")
-    loader = PDFReader()
-    documents = loader.load_data(file=Path('./docs/document.pdf'))
-    node_parser = SimpleNodeParser.from_defaults(chunk_size=500, chunk_overlap=20)
-    nodes = node_parser.get_nodes_from_documents(documents=documents)
+    documents = SimpleDirectoryReader("docs/txt").load_data()
 
     llm = OpenAI(model="gpt-3.5-turbo", temperature=0)
     embed_model = OpenAIEmbedding(model="text-embedding-ada-002", embed_batch_size=100)
     service_context = ServiceContext.from_defaults(
-        llm=llm, embed_model=embed_model, node_parser=node_parser
+        llm=llm, embed_model=embed_model
     )
 
     index_name = "documentation-db"
